b7713c05ca4a7877a8510d14cf17706492863528
Thong Tran <thongt@dlu.edu.vn>
2020-09-14 15:06:42 +0700

    chuong trinh dau tien

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Khai báo thư viện sử dụng  giống như iostream
+/* chú thích block*/
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
