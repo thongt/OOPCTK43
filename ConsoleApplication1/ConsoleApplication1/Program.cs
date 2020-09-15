d067bf13f4729ef6dc14184ce6f6fbf6ac7039d9
Thong Tran <thongt@dlu.edu.vn>
2020-09-15 07:03:45 +0700

    Chuong trinh dau tien

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Khai báo thư viện sử dụng  giống như iostream
 /* chú thích block*/
+
 using System;
 
 namespace ConsoleApplication1
@@ -8,7 +9,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //Xuat mot chuoi ra man hinh giong cout
 
+            //System.Console.WriteLine("Chuong trinh dau tien"); //dong lệnh
+
+            //Console.WriteLine("Dung khai bao thu vien thi ngan hon");
+
+            float tam = 7000000000;
+
+            int danso = (int)tam; // khai bao mot bien i kieu integer va khoi gan = 10
+            Console.WriteLine(" Dan so the gioi la " + tam);
+            Console.ReadKey();
         }
     }
 }
